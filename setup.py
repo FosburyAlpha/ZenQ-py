a244e5fac3fed8956e2a862d9e770055c177713e
RomeoV <romeovirgilio@gmail.com>
2024-12-15 10:50:27 +0100

    updated package

--- setup.py
+++ setup.py
@@ -1,7 +1,16 @@
 from setuptools import setup, find_packages
 setup(
-    name='zenq-py-lib',
-    version='0.1',
+    name='zenqapi',
+    version='0.11',
+    author="Fosbury Alpha Team",
+author_email="fosbury.alpha@gmail.com",
+description="ZenQ exchange python client.",
     packages=find_packages(),
     install_requires=['requests'],
+classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+python_requires=">=3.6",
 )
